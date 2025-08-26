092c07dec385b6a912354d59b8ed151b9500cd2e
Ubuntu <sai-user@vm1.mpwfwr1qqlfepg2di1rbgqyqmh.ux.internal.cloudapp.net>
2025-08-26 19:35:46 +0000

    Changed app service name in Terraform

--- infra/main.tf
+++ infra/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_service_plan" "plan" {
 
 resource "azurerm_linux_web_app" "web" {
   resource_group_name = azurerm_resource_group.lucky.name
-  name                = "lucky-web-app"
+  name                = "lucky-web-app-v2"
   location            = azurerm_resource_group.lucky.location
   service_plan_id     = azurerm_service_plan.plan.id
   site_config {
