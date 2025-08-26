1af747e699dee27a03daea5e974472c6ca534897
Ubuntu <sai-user@vm1.fxibb4cwqpkezkqfeefzvjkolc.ux.internal.cloudapp.net>
2025-08-26 16:37:17 +0000

    Add empty backend.tf file

--- infra/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "john"  # same as above
-    storage_account_name = "yourstorageacct"  # must be globally unique
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
-
--- infra/main.tf
+++ infra/main.tf
@@ -1,42 +1,40 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
       version = "4.30.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
+  features{}
   subscription_id = "7f44b397-03e8-463f-bcc4-9c1b2dcf4eac"
 }
 
 resource "azurerm_resource_group" "lucky" {
   name     = "john"
-  location = "North Europe"
+  location = "northeurope"
 }
 
 resource "azurerm_service_plan" "plan" {
+  resource_group_name = azurerm_resource_group.lucky.name
   name                = "lucky-service-plan"
   location            = azurerm_resource_group.lucky.location
-  resource_group_name = azurerm_resource_group.lucky.name
   os_type             = "Linux"
   sku_name            = "S1"
 }
 
 resource "azurerm_linux_web_app" "web" {
+  resource_group_name = azurerm_resource_group.lucky.name
   name                = "lucky-web-app"
   location            = azurerm_resource_group.lucky.location
-  resource_group_name = azurerm_resource_group.lucky.name
   service_plan_id     = azurerm_service_plan.plan.id
-
   site_config {
     application_stack {
-      java_server         = "JAVA"
-      java_version        = "21"
+      java_server       = "JAVA"
+      java_version      = "21"
       java_server_version = "21"
     }
   }
 }
-
